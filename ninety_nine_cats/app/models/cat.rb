9c89932b9e7c96dd2b4e6027ab6908de1f91bb95
le-s <steven.an.le@gmail.com>
2018-11-13 14:21:09 -0800

    Initial commit

---
+++ ninety_nine_cats/app/models/cat.rb
@@ -0,0 +1,27 @@
+# == Schema Information
+#
+# Table name: cats
+#
+#  id          :bigint(8)        not null, primary key
+#  birth_date  :date             not null
+#  color       :string           not null
+#  name        :string           not null
+#  sex         :string(1)        not null
+#  description :text             not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+class Cat < ApplicationRecord
+  include ActionView::Helpers::DateHelper
+  COLORS = ['red', 'black', 'white', 'orange', 'green', 'yellow']
+  
+  validates :birth_date, :color, :name, :sex, :description, presence: true
+  validates :color, inclusion: { in: COLORS, message: "Not a valid color" } 
+  
+  def age
+    # debugger
+    time_ago_in_words(birth_date)
+  end
+   
+end
